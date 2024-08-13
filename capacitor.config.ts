ef8799c6e72bf62f7b2b3b8378f0363a83c04fff
Sean Romocki <sean.romocki@asurion.com>
2024-08-13 18:01:30 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FreightWise Weather',
+  webDir: 'dist'
+};
+
+export default config;
